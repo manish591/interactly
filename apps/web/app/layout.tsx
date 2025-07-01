3545f1fa3b94206032408f71f1c9dd2bcb2f466c
Manish Devrani <manishdevrani777@gmail.com>
2025-07-01 22:44:59 +0530

    cleanup

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,16 +1,11 @@
-import { Geist, Geist_Mono } from 'next/font/google';
-
 import '@workspace/ui/globals.css';
+import { Manrope } from 'next/font/google';
 import { Providers } from '@/components/providers';
 
-const fontSans = Geist({
+const font = Manrope({
   subsets: ['latin'],
-  variable: '--font-sans',
-});
-
-const fontMono = Geist_Mono({
-  subsets: ['latin'],
-  variable: '--font-mono',
+  display: 'swap',
+  weight: ['700', '500'],
 });
 
 export default function RootLayout({
@@ -20,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
-      >
+      <body className={`${font.className} lowercase font-sans antialiased `}>
         <Providers>{children}</Providers>
       </body>
     </html>
---
+++ apps/web/components/header.tsx
@@ -0,0 +1,7 @@
+export function Header() {
+  return (
+    <header>
+      <h1>this is my header</h1>
+    </header>
+  );
+}
